affe94e5d55a57e422e152d6b86b95e7130db0d7
Akshata72 <sableakshata880@gmail.com>
2022-03-13 14:31:44 +0530

    Added:Swap the Numbers

---
+++ Swap_Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SwapNumbers
+{
+    class SwapNumbers
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Variable a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a Variable b:");   
+            int b = int.Parse(Console.ReadLine());
+            //a = a * b;
+            //b = a / b;
+            //a = a / b;
+            // Or
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("b=" +b);
+            Console.WriteLine("a=" +a);
+
+        }
+    }
+
+}
